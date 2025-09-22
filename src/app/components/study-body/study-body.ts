2466b3c763c584863f7384a04b422d06a3ea0388
412328_Rodriguez_Leonardo <412328@tecnicatura.frc.utn.edu.ar>
2025-09-22 01:45:36 -0300

    adicion de pantalla de estudios

--- src/app/components/study-body/study-body.ts
+++ src/app/components/study-body/study-body.ts
@@ -1,16 +1,18 @@
 import { Component } from '@angular/core';
 import {Card} from '../card/card';
 import {MatIcon} from '@angular/material/icon';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-study-body',
   imports: [
     Card,
-    MatIcon
+    MatIcon,
+    RouterLink
   ],
   templateUrl: './study-body.html',
   styleUrl: './study-body.css'
 })
 export class StudyBody {
-
+  gap: "sm" | "md" | "lg" = "md";
 }
